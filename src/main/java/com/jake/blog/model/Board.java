1fbda22ec716f7cd1b7da657843a3daad7afd101
jakebae87 <jeonghyun.bae@kakao.com>
2022-11-09 22:09:03 +0900

    Board 테이블 생성

--- src/main/java/com/jake/blog/model/Board.java
+++ src/main/java/com/jake/blog/model/Board.java
@@ -1,4 +1,31 @@
 package com.jake.blog.model;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ColumnDefault("0")
+    private int count; //조회수
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user; // FK 설정
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
