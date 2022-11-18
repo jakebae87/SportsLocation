8bdbb1b8ce21b209f8ed2bf1c8dae9159ce2b441
jakebae87 <jeonghyun.bae@kakao.com>
2022-11-18 13:55:00 +0900

    index 페이지에 글 목록표시 구현

--- src/main/java/com/jake/blog/controller/BoardController.java
+++ src/main/java/com/jake/blog/controller/BoardController.java
@@ -1,15 +1,22 @@
 package com.jake.blog.controller;
 
 import com.jake.blog.config.auth.PrincipalDetail;
+import com.jake.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping({"", "/"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("boards",boardService.list());
         return "index";
     }
 
